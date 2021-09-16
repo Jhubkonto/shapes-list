f82e04f7301cd7b7e2d058917bb8e73503bf2851
Jhubkonto <julian.lacis@gmail.com>
2021-09-17 01:34:33 +0200

    Edited button styling

--- src/components/TriangleShape/TriangleShape.js
+++ src/components/TriangleShape/TriangleShape.js
@@ -6,6 +6,7 @@ export const TriangleShape = () => {
         xmlns="http://www.w3.org/2000/svg" 
         width="96" 
         height="96" 
+        fill="rgb(0, 110, 255)"
         viewBox="0 0 24 24"><defs/>
         <path fill="none" d="M0 0h24v24H0V0z"/>
         <path d="M12 7.77L18.39 18H5.61L12 7.77M12 4L2 20h20L12 4z"/>
